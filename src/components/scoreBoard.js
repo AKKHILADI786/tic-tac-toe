d8e01f221a77c6393ef5f21528ca5c31d06ce052
AKSHAY KUMAR VERMA <akshaykumarnitj@gmail.com>
2021-05-14 14:38:24 +0530

    Phone UI and Readme Updated

--- src/components/scoreBoard.js
+++ src/components/scoreBoard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Score from './score'
 export default function ScoreBoard({win,loss,tie}) {
     return (
-        <div className="container">
+        <div className="container scoreboard-box">
              <div className="board-row score-board row">
             <Score name={'Player'} val={win} />
             <Score name={'Tie'} val={tie} />
